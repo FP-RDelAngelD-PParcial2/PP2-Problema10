4767bf1be75580ae837ddda0a1b2b9cf2ab52a8c
PC <PC@192.168.1.70>
2016-10-13 22:37:36 -0500

    10.	Realiza un programa que muestra los N primeros números de la serie de Fibonacci.

--- src/pp2/problema10/PP2Problema10.java
+++ src/pp2/problema10/PP2Problema10.java
@@ -35,10 +35,10 @@ public class PP2Problema10 {
         System.out.print("Los primeros " + numero + " numeros de la serie Fibonacci son: ");
         
        f1=1;
-       f2=2;
-       System.out.print(f1 + "");
+       f2=1;
+       System.out.print(f1 + "\n");
        for(int i = 2;i<=numero;i++){
-           System.out.print(f2 + "");
+           System.out.print(f2 + "\n");
            f2 = f1 + f2;
            f1 = f2 - f1;
        }
